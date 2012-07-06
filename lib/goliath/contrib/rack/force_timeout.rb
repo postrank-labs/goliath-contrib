f68fbd8c50369101e31f6ca3e338835eba8f8008
Philip (flip) Kromer <flip@infochimps.org>
2012-07-06 00:15:17 -0500

    force timeout calls its response chain rather than raising an exception.  it now formulates the response in the timeout condition directly, and calls its middleware predecessor with that error reposnse. This was always the intended behavior, but a too-helpful rescue block was making it skip the remaining middleware chain.

--- lib/goliath/contrib/rack/force_timeout.rb
+++ lib/goliath/contrib/rack/force_timeout.rb
@@ -5,6 +5,18 @@ module Goliath
       #
       # Force a timeout after given number of seconds
       #
+      # ForceTimeout ensures your response takes *at most* N seconds. ForceDelay
+      # ensures your response takes *at least* N seconds. To have a response
+      # take *as-close-as-reasonable-to* N seconds, use an N-second ForceTimeout
+      # with an (N+1)-second ForceDelay.
+      #
+      #
+      # @example first call is 200 OK, second will error with 408 RequestTimeoutError (see examples/test_rig.rb):
+      #   curl -v 'http://127.0.0.1:9000/?_force_timeout=1.0&_force_delay=0.5'
+      #   => Headers: X-Resp-Delay: 0.5 / X-Resp-Randelay: 0.0 / X-Resp-Actual: 0.513401985168457 / X-Resp-Timeout: 1.0
+      #   curl -v 'http://127.0.0.1:9000/?_force_timeout=1.0&_force_delay=2.0'
+      #   => {"status":408,"error":"RequestTimeoutError","message":"Request exceeded 1.0 seconds"}
+      #
       class ForceTimeout
         include Goliath::Rack::Validator
 
@@ -28,6 +40,7 @@ module Goliath
             env['async.callback'] = Proc.new do |status, headers, body|
               unless env[:force_timeout_complete]
                 env[:force_timeout_complete] = true
+                headers.merge!('X-Resp-Timeout' => timeout.to_s)
                 async_cb.call([status, headers, body])
               end
             end
@@ -36,7 +49,11 @@ module Goliath
             # This will always fire, we just don't do anything if already handled.
             # If not handled elsewhere, mark as handled and raise an error
             EM.add_timer(timeout) do
-              async_cb.call(safely(env){ handle_timeout(env, timeout) })
+              unless env[:force_timeout_complete]
+                env[:force_timeout_complete] = true
+                err = Goliath::Validation::RequestTimeoutError.new("Request exceeded #{timeout} seconds")
+                async_cb.call(error_response(err, 'X-Resp-Timeout' => timeout.to_s))
+              end
             end
           end
 
@@ -48,12 +65,6 @@ module Goliath
           [status, headers, body]
         end
 
-        def handle_timeout(env, timeout)
-          unless env[:force_timeout_complete]
-            env[:force_timeout_complete] = true
-            raise Goliath::Validation::RequestTimeoutError.new("Request exceeded #{timeout} seconds")
-          end
-        end
       end
     end
   end
