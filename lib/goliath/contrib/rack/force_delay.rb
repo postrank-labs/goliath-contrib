3a24739d7066bd75b5a6153d43ddfe0a6f5e5c80
Philip (flip) Kromer <flip@infochimps.org>
2012-07-06 00:16:16 -0500

    more clearer the documentation was made to be

--- lib/goliath/contrib/rack/force_delay.rb
+++ lib/goliath/contrib/rack/force_delay.rb
@@ -3,18 +3,25 @@ module Goliath
 
     module Rack
 
-      # Delays response for `_delay + (0 to _randelay)` seconds, as given by the
-      # `_delay` and `_randelay` request parameters. Delays longer than 5
-      # seconds are clamped to 5 seconds.
+      # Delays response for `delay + (0 to randelay)` additional seconds after
+      # the app's response.
       #
       # This delay is non-blocking -- *other* requests may proceed in turn --
       # though naturally the call chain for this response doesn't proceed until
       # the delay is complete.
       #
-      # Information about the delay is added to the response headers for your enjoyment.
+      # ForceDelay ensures your response takes *at least* N seconds. Force
+      # Timeout ensures your response takes *at most* N seconds. To have a
+      # response take *as-close-as-reasonable-to* N seconds, use an N-second
+      # ForceTimeout with an (N+1)-second ForceDelay.
       #
-      # @example simulate a highly variable response time (min 0.5, max 1.5 seconds)
-      #   curl -v 'http://127.0.0.1:9000/?_delay=0.5&_randelay=1.0'
+      # The `force_delay` and `force_randelay` env variables specify the delay;
+      # values are clamped to be less than 5 seconds. Information about the
+      # delay is added to the response headers for your enjoyment.
+      #
+      # @example simulate a highly variable (0.5-1.5 sec) response time (see examples/test_rig.rb):
+      #   curl -v 'http://127.0.0.1:9000/?_force_delay=0.5&_force_randelay=1.0'
+      #   => Headers: X-Resp-Delay: 0.5 / X-Resp-Randelay: 1.0 / X-Resp-Actual: 0.90205979347229
       #
       class ForceDelay
         include Goliath::Rack::AsyncMiddleware
@@ -27,7 +34,6 @@ module Goliath
             be_sleepy(delay, randelay)
             actual = (Time.now.to_f - env[:start_time])
             headers.merge!( 'X-Resp-Delay' => delay.to_s, 'X-Resp-Randelay' => randelay.to_s, 'X-Resp-Actual' => actual.to_s )
-            body.merge!( :_delay_ms => delay, :_randelay_ms => randelay, :_actual_ms => actual, :_delay_start => env[:start_time].to_f )
           end
           [status, headers, body]
         end
--- lib/goliath/contrib/rack/force_drop.rb
+++ lib/goliath/contrib/rack/force_drop.rb
@@ -4,14 +4,18 @@ module Goliath
 
       # Middleware to simulate dropping a connection.
       #
-      # * if the _drop       env var is given, close the connection as soon as possible
-      # * if the _drop_after env var is given, close the connection late (after all following middlewares have happened)
+      # * if the force_drop       env var is given, close the connection as soon as possible
+      # * if the force_drop_after env var is given, close the connection late (after all following middlewares have happened)
       #
-      # @example drop the connection immediately
-      #   curl 'http://localhost:9000/?_drop=true'
+      # @example drop the connection immediately (see examples/test_rig.rb):
+      #   time curl 'http://localhost:9000/?_force_drop=true'
+      #   => curl: (52) Empty reply from server
+      #   real  0m0.027s        user    0m0.008s        sys     0m0.005s
       #
-      # @example wait one second then drop the connection with no response (note: `_drop_after`, not `_drop`)
-      #   curl 'http://localhost:9000/?_drop_after=true&_delay=1'
+      # @example drop the connection with no response after waiting one second; the delay is provided by the `ForceDelay` middleware in `_drop_after` mode:
+      #   time curl 'http://localhost:9000/?_drop_after=true&_delay=1'
+      #   => curl: (52) Empty reply from server
+      #   real  0m1.111s        user    0m0.008s        sys     0m0.005s
       #
       class ForceDrop
         include Goliath::Rack::AsyncMiddleware
